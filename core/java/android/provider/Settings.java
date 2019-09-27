6eca49515562493c7c71f1ce8f6b2e6760f8a91b
maxwen <max.weninger@gmail.com>
2019-09-27 23:13:28 +0200

    base: add CPU info overlay
    includes all modifications and fixes from P
    
    Change-Id: I0e6dee53ebcd864a522cfd3d78a5abb253991a9c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4783,6 +4783,15 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * @hide
+         */
+        public static final String OMNI_SHOW_CPU_OVERLAY = "show_cpu_overlay";
+
+        /** @hide */
+        private static final Validator OMNI_SHOW_CPU_OVERLAY_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
          * Whether to display the 4g or lte network icon in the statusbar
          * @hide
          */
@@ -4875,6 +4884,7 @@ public final class Settings {
             OMNI_BACK_GESTURE_HEIGHT,
             OMNI_DOUBLE_TAP_SLEEP_GESTURE,
             OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN,
+            OMNI_SHOW_CPU_OVERLAY,
         };
 
         /**
@@ -5029,6 +5039,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(OMNI_BACK_GESTURE_HEIGHT);
             PRIVATE_SETTINGS.add(OMNI_DOUBLE_TAP_SLEEP_GESTURE);
             PRIVATE_SETTINGS.add(OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN);
+            PRIVATE_SETTINGS.add(OMNI_SHOW_CPU_OVERLAY);
         }
 
         /**
@@ -5151,6 +5162,8 @@ public final class Settings {
                     OMNI_DOUBLE_TAP_SLEEP_GESTURE_VALIDATOR);
             VALIDATORS.put(OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN,
                     OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN_VALIDATOR);
+            VALIDATORS.put(OMNI_SHOW_CPU_OVERLAY,
+                    OMNI_SHOW_CPU_OVERLAY_VALIDATOR);
         }
 
         /**
